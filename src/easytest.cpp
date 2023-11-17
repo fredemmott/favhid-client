a24eb44b5ab41ca3e3712408e342d22b9ef5b4e0
Fred Emmott <git@fredemmott.com>
2023-11-16 21:50:42 -0600

    Add a HID report builder

--- src/easytest.cpp
+++ src/easytest.cpp
@@ -1,106 +1,95 @@
 // Copyright 2023 Fred Emmott <fred@fredemmott.com>
 // SPDX-License-Identifier: ISC
 
+#include "favhid/descriptors.hpp"
+#include "favhid/io.hpp"
+#include "favhid/protocol.hpp"
+
 #include <Windows.h>
 
 #include <winrt/base.h>
 
-#include <iostream>
-#include <format>
-#include <wbemidl.h>
-
 #include <cstdint>
+#include <format>
+#include <iostream>
 
-#include "favhid/protocol.hpp"
-#include "favhid/io.hpp"
+#include <wbemidl.h>
 
 using namespace FAVHID;
 
 constexpr uint8_t REPORT_ID = 0x03;
 
-constexpr uint8_t HID_DESCRIPTOR[] = {
-// clang-format off
-    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-    0x09, 0x05,                    // USAGE (Game Pad)
-    0xa1, 0x01,                    // COLLECTION (Application)
-    0xa1, 0x00,                    //   COLLECTION (Physical)
-    // ReportID - 8 bits
-    0x85, REPORT_ID,                    //     REPORT_ID (REPORT_ID)
-    // X & Y - 2x8 = 16 bits
-    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
-    0x09, 0x30,                    //     USAGE (X)
-    0x09, 0x31,                    //     USAGE (Y)
-    0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
-    0x25, 0x7f,                    //     LOGICAL_MAXIMUM (127)
-    0x75, 0x08,                    //     REPORT_SIZE (8)
-    0x95, 0x02,                    //     REPORT_COUNT (2)
-    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
-    // Buttons - 8 bits
-    0x05, 0x09,                    //     USAGE_PAGE (Button)
-    0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
-    0x29, 0x08,                    //     USAGE_MAXIMUM (Button 8)
-    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
-    0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
-    0x75, 0x01,                    //     REPORT_SIZE (1)
-    0x95, 0x08,                    //     REPORT_COUNT (8)
-    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
-    0xc0,                          //     END_COLLECTION
-    0xc0                           // END_COLLECTION
-// clang-format on
-};
-
 #pragma pack(push, 1)
 struct Report {
-    uint8_t x {};
-    uint8_t y {};
-    uint8_t buttons {};
+  uint8_t x {};
+  uint8_t y {};
+  uint8_t buttons {};
 };
 #pragma pack(pop)
 
+static void WriteDescriptor(const THandle& handle, uint8_t reportID) {
+  using namespace FAVHID::Descriptors;
+
+  FAVHID::Descriptors::Descriptor desc {
+    UsagePage::GenericDesktop(),
+    Usage::GamePad(),
+    Collection::Application(Collection::Physical(
+      ReportID(reportID),
+      UsagePage::GenericDesktop(),
+      Usage::X(),
+      Usage::Y(),
+      LogicalMinimum::ForType<int8_t>(),
+      LogicalMaximum::ForType<int8_t>(),
+      ReportSize(8),
+      ReportCount(2),
+      Input::DataVariableAbsolute(),
+      UsagePage::Button(),
+      UsageMinimum(1),
+      UsageMaximum(8),
+      LogicalMinimum(0),
+      LogicalMaximum(1),
+      ReportSize(1),
+      ReportCount(8),
+      Input::DataVariableAbsolute()))};
+
+  Report report {};
+  WriteDescriptor(
+    handle, reportID, desc.data(), desc.size(), &report, sizeof(report));
+}
 
 int main() {
-    std::cout << "Opening port..." << std::endl;
-    auto f = OpenArduino();
+  std::cout << "Opening port..." << std::endl;
+  auto f = OpenArduino();
 
-    if (!f) {
-        std::cout << "Failed to find arduino." << std::endl;
-        return 1;
+  if (!f) {
+    std::cout << "Failed to find arduino." << std::endl;
+    return 1;
+  }
+
+  WriteDescriptor(f, REPORT_ID);
+
+  f.close();
+  std::cout << "Waiting to re-open" << std::endl;
+  Sleep(500);
+  f = OpenArduino();
+  std::cout << "Re-opened, feeding" << std::endl;
+
+  if (!f) {
+    return 1;
+  }
+
+  Report report {.buttons = 1};
+  while (true) {
+    Sleep(250);
+    report.buttons <<= 1;
+    if (!report.buttons) {
+      report.buttons = 1;
     }
-
-    {
-        constexpr Report report {
-            .x = 128,
-            .y = 128,
-            .buttons = 0xff,
-        };
-
-        auto response = WriteDescriptor(f, REPORT_ID, HID_DESCRIPTOR, sizeof(HID_DESCRIPTOR), &report, sizeof(report));
-        if (!response.IsOK()) {
-            std::cout << "Failed to put device" << std::endl;
-        }
+    const auto result = WriteReport(f, REPORT_ID, &report, sizeof(report));
+    if (!result.IsOK()) {
+      __debugbreak();
     }
+  }
 
-    f.close();
-    std::cout << "Waiting to re-open" << std::endl;
-    Sleep(500);
-    f = OpenArduino();
-
-    if (!f) {
-        return 1;
-    }
-
-    Report report { .buttons = 1 };
-    while(true) {
-        Sleep(250);
-        report.buttons <<= 1;
-        if (!report.buttons) {
-            report.buttons = 1;
-        }
-        const auto result = WriteReport(f, REPORT_ID, &report, sizeof(report));
-        if (!result.IsOK()) {
-            __debugbreak();
-        }
-    }
-
-    return 0;
+  return 0;
 }
---
+++ src/favhid/descriptors.hpp
@@ -0,0 +1,213 @@
+// Copyright 2023 Fred Emmott <fred@fredemmott.com>
+// SPDX-License-Identifier: ISC
+
+#pragma once
+
+#include <bit>
+#include <cinttypes>
+#include <concepts>
+#include <limits>
+#include <string_view>
+
+namespace FAVHID::Descriptors {
+
+class Entry {
+ public:
+  std::basic_string_view<uint8_t> GetBytes() const {
+    return mSerialized;
+  }
+
+ protected:
+  Entry() = delete;
+  Entry(std::basic_string_view<uint8_t> value) {
+    mSerialized = std::basic_string<uint8_t> {value};
+  }
+
+  Entry(uint8_t value) : Entry({&value, 1}) {
+  }
+
+  Entry(uint16_t value) : Entry({reinterpret_cast<uint8_t*>(&value), 2}) {
+  }
+
+  Entry(uint8_t a, uint8_t b) : Entry(static_cast<uint16_t>((b << 8) | a)) {
+  }
+
+  std::basic_string<uint8_t> mSerialized;
+};
+
+class UsagePage final : public Entry {
+ public:
+  UsagePage(uint8_t value) : Entry(0x05, value) {
+  }
+
+  static UsagePage GenericDesktop() {
+    return {0x01};
+  }
+  static UsagePage Button() {
+    return {0x09};
+  }
+};
+
+class Usage final : public Entry {
+ public:
+  Usage(uint8_t value) : Entry(0x09, value) {
+  }
+
+  static Usage GamePad() {
+    return {0x05};
+  }
+
+  static Usage X() {
+    return {0x30};
+  }
+
+  static Usage Y() {
+    return {0x31};
+  }
+};
+
+class UsageMinimum final : public Entry {
+ public:
+  UsageMinimum(auto value) : Entry(0x19, value) {
+  }
+};
+
+class UsageMaximum final : public Entry {
+ public:
+  UsageMaximum(auto value) : Entry(0x29, value) {
+  }
+};
+
+class Collection final : public Entry {
+ public:
+  template <std::derived_from<Entry>... Ts>
+  Collection(uint8_t id, const Ts&... ts) : Entry(0xa1, id) {
+    ([&]() { mSerialized += ts.GetBytes(); }(), ...);
+    mSerialized += '\xc0';
+  }
+
+  template <std::convertible_to<Entry>... Ts>
+  static Collection Physical(const Ts&... ts) {
+    return {0x00, ts...};
+  }
+
+  template <std::convertible_to<Entry>... Ts>
+  static Collection Application(const Ts&... ts) {
+    return {0x01, ts...};
+  }
+};
+
+class ReportID final : public Entry {
+ public:
+  ReportID(uint8_t value) : Entry(0x85, value) {
+  }
+};
+
+// TODO: figure out multi-byte stuff
+class IntValueEntry : public Entry {
+ public:
+  IntValueEntry(uint8_t id, auto value) : Entry(id) {
+    if (value < 0) {
+      mSerialized += std::bit_cast<char>(static_cast<int8_t>(value));
+    } else {
+      mSerialized += static_cast<char>(value);
+    }
+  }
+};
+
+class ReportSize final : public IntValueEntry {
+ public:
+  ReportSize(auto value) : IntValueEntry(0x75, value) {
+  }
+};
+
+class ReportCount final : public IntValueEntry {
+ public:
+  ReportCount(auto value) : IntValueEntry(0x95, value) {
+  }
+};
+
+class LogicalMinimum final : public IntValueEntry {
+ public:
+  LogicalMinimum(auto value) : IntValueEntry(0x15, value) {
+  }
+
+  template <class T>
+  static LogicalMinimum ForType() {
+    return {std::numeric_limits<T>::min()};
+  }
+};
+
+class LogicalMaximum final : public IntValueEntry {
+ public:
+  LogicalMaximum(auto value) : IntValueEntry(0x25, value) {
+  }
+
+  template <class T>
+  static LogicalMaximum ForType() {
+    return {std::numeric_limits<T>::max()};
+  }
+};
+
+// TODO: add support for multibyte flags
+class Input final : public IntValueEntry {
+ public:
+  Input(auto flags) : IntValueEntry(0x81, flags) {
+  }
+
+  using Flag = uint8_t;
+
+  // static constexpr Flag Data = 0;
+  static constexpr Flag Constant = 1;
+
+  // static constexpr Flag Array = 0;
+  static constexpr Flag Variable = 1 << 1;
+
+  // static constexpr Flag Absolute = 0;
+  static constexpr Flag Relative = 1 << 2;
+
+  // static constexpr Flag NoWrap = 0;
+  static constexpr Flag Wrap = 1 << 3;
+
+  // static constexpr Flag Linear = 0;
+  static constexpr Flag NonLinear = 1 << 4;
+
+  // static constexpr Flag PreferredState = 0;
+  static constexpr Flag NoPreferredState = 1 << 5;
+
+  // static constexpr Flag NoNullPosition = 0;
+  static constexpr Flag NullState = 1 << 6;
+
+  // static constexpr Flag NonVolatile = 0;
+  static constexpr Flag Volatile = 1 << 7;
+
+  static Input DataVariableAbsolute() {
+    return {Variable};
+  }
+  static Input Padding() {
+    return {Constant};
+  }
+};
+
+class Descriptor final {
+ public:
+  Descriptor() = delete;
+
+  template <std::derived_from<Entry>... Ts>
+  Descriptor(const Ts&... entries) {
+    ([&]() { mSerialized += entries.GetBytes(); }(), ...);
+  }
+
+  const void* data() const {
+    return mSerialized.data();
+  }
+
+  size_t size() const {
+    return mSerialized.size();
+  }
+
+ private:
+  std::basic_string<uint8_t> mSerialized;
+};
+
+}// namespace FAVHID::Descriptors
