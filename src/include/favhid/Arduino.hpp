2d9abbd7732b1c03e6e02578208013ee86bf0ef8
Fred Emmott <git@fredemmott.com>
2023-11-17 09:48:40 -0600

    Add usage documentation

--- src/include/favhid/Arduino.hpp
+++ src/include/favhid/Arduino.hpp
@@ -22,6 +22,38 @@ struct Response {
   }
 };
 
+/** The primary C++ API for FAVHID
+ *
+ * 
+ * One-off Preparation
+ * ===================
+ * 
+ * Generate a GUID or OpaqueID for your configuration; this is used to
+ * detect when the Arduino needs to be rebooted to get a fresh start.
+ * 
+ * You can use the Visual Studio GUID generator for this, or the
+ * `generate-random-ids` tool from this repository.
+ * 
+ * This ID should be the same between runs, unless you use different
+ * HID descriptors in different runs.
+ * 
+ * Usage
+ * =====
+ * 
+ * 1. Open() an Arduino. Specifying a serial number is recommended, but
+ *    optional. Serial numbers are unique to FAVHID.
+ * 2. Check if `GetVolatileConfigID()` matches the `OpaqueID` you created
+ *    earlier; if so, jump to step 3; otherwise, you need to initialize the
+ *    device:
+ *    2.1) Call `HardReset()` to reset the device and clear RAM
+ *    2.2) Call `PushDescriptor()` for each of your descriptors
+ *    2.3) Call `SetVolatileConfigID()`, passing in the `OpaqueID` you
+ *      created earlier.
+ *    2.4) Call `ResetUSB()` so that the OS picks up the new descriptors
+ *    2.5) Call `GetVolatileConfigID()` now matches the `OpaqueID` you
+ *      provided
+ * 3. Feed: Call `WriteReport()` to push new data.
+ */
 class Arduino final {
  public:
   Arduino() = delete;
