ae7eaad6e10deeeb3c71368107e1f2352f0968e0
Fred Emmott <git@fredemmott.com>
2023-11-17 10:08:55 -0600

    Dump code from generate-random-ids

--- src/generate-random-ids.cpp
+++ src/generate-random-ids.cpp
@@ -3,13 +3,36 @@
 
 #include "favhid/protocol.hpp"
 
+#include <format>
 #include <iostream>
 
 using namespace FAVHID;
 
 int main() {
-    for (int i = 0; i < 10; ++i) {
-        std::cout << OpaqueID::Random().HumanReadable() << std::endl;
-    }
-    return 0;
+  for (int i = 0; i < 10; ++i) {
+    const auto id = OpaqueID::Random();
+    std::cout << "// " << id.HumanReadable() << std::endl;
+    std::cout << std::format(
+      "constexpr UniqueID MY_ID {{\n"
+      "  0x{:08x},\n"
+      "  0x{:04x},\n"
+      "  0x{:04x},\n"
+      "  {{"
+      "0x{:02x}, 0x{:02x}, 0x{:02x}, 0x{:02x}, "
+      "0x{:02x}, 0x{:02x}, 0x{:02x}, 0x{:02x}}},\n"
+      "}};",
+      id.Data1,
+      id.Data2,
+      id.Data3,
+      id.Data4[0],
+      id.Data4[1],
+      id.Data4[2],
+      id.Data4[3],
+      id.Data4[4],
+      id.Data4[5],
+      id.Data4[6],
+      id.Data4[7])
+              << std::endl;
+  }
+  return 0;
 }
